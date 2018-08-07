02cafa09bc07e5489e1ba6730724bd9867e54125
闫孟亭 <wb-ymt409060@alibaba-inc.com>
2018-08-07 10:43:10 +0800

    fix

--- my_web/src/main/java/com/ymt/Application.java
+++ my_web/src/main/java/com/ymt/Application.java
@@ -1,13 +1,15 @@
-package org.my_web;
+package com.ymt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
 @EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.ymt"})
 public class Application {
     public static void main( String[] args ){
     	 SpringApplication.run(Application.class, args);
--- my_web/src/main/java/com/ymt/controller/MainController.java
+++ my_web/src/main/java/com/ymt/controller/MainController.java
@@ -1,4 +1,4 @@
-package org.my_web.controller;
+package com.ymt.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
